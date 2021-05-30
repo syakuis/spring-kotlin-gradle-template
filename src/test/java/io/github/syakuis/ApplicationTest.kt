f037447f7f07daed3a93f2eb46accd54c19f9221
syaku <syaku@naver.com>
2021-05-30 20:25:38 +0900

    kotlin 설정 수정 kotest 추가

--- src/test/java/io/github/syakuis/ApplicationTest.kt
+++ src/test/java/io/github/syakuis/ApplicationTest.kt
@@ -1,37 +1,42 @@
 package io.github.syakuis;
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.support.MessageSourceAccessor
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import java.util.*
 
-@ExtendWith(SpringExtension::class)
 @SpringBootTest
-class ApplicationTest {
+internal class ApplicationTest : BehaviorSpec() {
     private val log = LoggerFactory.getLogger(this.javaClass)
 
     @Autowired
-    private lateinit var i18n: MessageSourceAccessor;
+    private lateinit var i18n: MessageSourceAccessor
 
     @Autowired
-    private lateinit var objectMapper: ObjectMapper;
+    private lateinit var objectMapper: ObjectMapper
 
-    @Test
-    fun `국제화 메세지`() {
-        val name = i18n.getMessage("author.name");
-        Assertions.assertEquals("Seokkyun. Choi.", name);
-        log.debug(name);
-    }
+    init {
+        Given("스프링 부트 테스트") {
+            When("국제화 메세지를 가져온다.") {
+                val name = i18n.getMessage("author.name")
 
-    @Test
-    fun `jackson`() {
-        val json = "[1,2,3,4]";
-        Assertions.assertEquals(listOf(1, 2, 3, 4), objectMapper.readValue(json, ArrayList::class.java));
+                Then("메세지를 정상적으로 가져왔다.") {
+                    "Seokkyun. Choi." shouldBe name
+                }
+
+            }
+
+            When("jackson 값을 설정한다.") {
+                val json = "[1,2,3,4]"
+
+                Then("값을 비교한다.") {
+                    listOf(1, 2, 3, 4) shouldBe objectMapper.readValue(json, ArrayList::class.java)
+                }
+            }
+        }
     }
 }

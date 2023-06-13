2432949ffe5e942f1d881fa00f8e2d4b9d9a8b7b
최석균 <syaku@choeseoggyun-ui-MacBookPro.local>
2023-06-13 23:59:41 +0900

    testing

--- src/main/java/io/github/syakuis/configuration/MessageSourceConfiguration.kt
+++ src/main/java/io/github/syakuis/configuration/MessageSourceConfiguration.kt
@@ -1,15 +1,26 @@
 package io.github.syakuis.configuration;
 
+import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.context.MessageSource
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
+import org.springframework.context.support.AbstractApplicationContext
 import org.springframework.context.support.MessageSourceAccessor
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean
 import java.util.*
 
-@Configuration(proxyBeanMethods = false)
-class MessageSourceConfiguration {
-    @Bean("i18n")
-    fun i18n(messageSource: MessageSource): MessageSourceAccessor {
+@ConditionalOnBean(name = [AbstractApplicationContext.MESSAGE_SOURCE_BEAN_NAME])
+@AutoConfiguration(after = [MessageSource::class])
+class MessageSourceConfiguration(private val messageSource: MessageSource) {
+    @Bean
+    fun messageSourceAccessor(): MessageSourceAccessor {
         return MessageSourceAccessor(messageSource, Locale.getDefault())
+    }
+
+    @Bean
+    fun validator(): LocalValidatorFactoryBean {
+        val bean = LocalValidatorFactoryBean()
+        bean.setValidationMessageSource(messageSource)
+        return bean
     }
 }
--- src/test/java/io/github/syakuis/ApplicationTest.kt
+++ src/test/java/io/github/syakuis/ApplicationTest.kt
@@ -2,27 +2,35 @@ package io.github.syakuis;
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.MessageSource
 import org.springframework.context.support.MessageSourceAccessor
-import java.util.*
 
 @SpringBootTest
 internal class ApplicationTest : BehaviorSpec() {
     private val log = LoggerFactory.getLogger(this.javaClass)
 
     @Autowired
-    private lateinit var i18n: MessageSourceAccessor
+    private lateinit var messageSource: MessageSource
+
+    @Autowired
+    private lateinit var messageSourceAccessor: MessageSourceAccessor
 
     @Autowired
     private lateinit var objectMapper: ObjectMapper
 
     init {
         Given("스프링 부트 테스트") {
+            Then("스프링 빈 생성됨.") {
+                messageSource shouldNotBe null
+            }
             When("국제화 메세지를 가져온다.") {
-                val name = i18n.getMessage("author.name")
+                val name = messageSourceAccessor.getMessage("author.name")
 
                 Then("메세지를 정상적으로 가져왔다.") {
                     "Seokkyun. Choi." shouldBe name
